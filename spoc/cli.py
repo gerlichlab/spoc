9c0a59de9545a766829f5811e6aa20a20b7ca23b
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2022-07-25 21:46:39 +0200

    added untested cli wrappers

--- spoc/cli.py
+++ spoc/cli.py
@@ -1,15 +1,36 @@
 """Console script for spoc."""
 import sys
 import click
+from spoc.contacts import ContactExpander
+from spoc.io import FileManager
+
+
+
+@click.group()
+def main(args=None):
+    """Console script for spoc."""
+    pass
 
 
 @click.command()
-def main(args=None):
-    """Console script for spoc."""
-    click.echo("Replace this message by putting your code into " "spoc.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+@click.argument('input')
+@click.argument('output')
+@click.option(
+    "-n",
+    "--n_fragments",
+    default=3,
+    help="Number of fragments per read to expand",
+)
+def expand(input, output, n_fragments):
+    """Script for expanding fragments to contacts"""
+    expander = ContactExpander(number_fragments=n_fragments)
+    file_manager = FileManager()
+    input_fragments = file_manager.load_porec_fragments(input)
+    expanded_contacts = expander.expand(input_fragments)
+    file_manager.write_multiway_contacts(output, expanded_contacts)
 
+
+main.add_command(expand)
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
