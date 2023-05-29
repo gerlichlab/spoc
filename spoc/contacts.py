40bcac89f2a902ea940b2d980960d9297e6f4023
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2023-05-29 18:34:50 +0200

    sorting labelled contacts

--- spoc/contacts.py
+++ spoc/contacts.py
@@ -5,8 +5,11 @@ from typing import List, Union
 import pandas as pd
 import dask.dataframe as dd
 from typing import Union, Optional
+from itertools import permutations, product
 from spoc.dataframe_models import ContactSchema
-from spoc.symmetry import LabelledSymmetryFlipper, UnlabelledSymmetryFlipper
+import numpy as np
+
+DataFrame = Union[pd.DataFrame, dd.DataFrame]
 
 
 class Contacts:
@@ -14,9 +17,10 @@ class Contacts:
 
     def __init__(
         self,
-        contact_frame: Union[pd.DataFrame, dd.DataFrame],
+        contact_frame: DataFrame,
         number_fragments: Optional[int] = None,
-        metadata_combi: Optional[List[str]] = None
+        metadata_combi: Optional[List[str]] = None,
+        label_sorted: bool = False,
     ) -> None:
         self.contains_meta_data = "meta_data_1" in contact_frame.columns # All contacts contain at least one fragment
         if number_fragments is None:
@@ -31,9 +35,10 @@ class Contacts:
         else:
             self.is_dask = True
         self._data = self._schema.validate(contact_frame)
-        self._metadata_combi = metadata_combi
+        self.metadata_combi = metadata_combi
+        self.label_sorted = label_sorted
 
-    def _guess_number_fragments(self, contact_frame: Union[pd.DataFrame, dd.DataFrame]) -> int:
+    def _guess_number_fragments(self, contact_frame: DataFrame) -> int:
         """Guesses the number of fragments from the contact frame"""
         return max(int(i.split("_")[1]) for i in contact_frame.columns if "start" in i)
 
@@ -48,20 +53,8 @@ class Contacts:
 
 
     def subset_on_metadata(self, metadata_combi:List[str]):
-        """Filters on metadata"""
+        """Filters on metadata and sorts label states"""
         # TODO
-
-    def flip_symmetric_contacts(self) -> Contacts:
-        """Flips contacts based on inherent symmetry"""
-        if self.contains_meta_data and self._metadata_combi is None:
-            raise ValueError("""Flipping symmetry is only supported for pure metadata combinations.
-                             Either subset or pass to constructor.""")
-        if self.contains_meta_data:
-            flipper = LabelledSymmetryFlipper(self._metadata_combi)
-        else:
-            flipper = UnlabelledSymmetryFlipper()
-        result = flipper.flip_contacts(self.data)
-        return Contacts(result, number_fragments=self.number_fragments)
 
     def __repr__(self) -> str:
         return f"<Contacts | order: {self.number_fragments} | contains metadata: {self.contains_meta_data}>"
@@ -91,3 +84,69 @@ class ContactManipulator:
         return Contacts(
             pd.concat([i.data for i in merge_list]), number_fragments=number_fragments
         )
+
+    @staticmethod
+    def _generate_rename_columns(order):
+        columns = ["chrom", "start", "end", "mapping_quality", "align_score", "align_base_qscore", "meta_data"]
+        rename_columns = {}
+        for i in range(len(order)):
+            for column in columns:
+                current_name = f"{column}_{i+1}"
+                new_name = f"{column}_{order.index(i+1) + 1}"
+                rename_columns[current_name] = new_name
+        return rename_columns
+
+    def _flip_unlabelled_contacts(self, df: DataFrame) -> DataFrame:
+        """Flips contacts"""
+        fragment_order = max(int(i.split("_")[1]) for i in df.columns if "start" in i)
+        subsets = []
+        for perm in permutations(range(1, fragment_order+1)):
+            query = "<=".join([f"start_{i}" for i in perm])
+            subsets.append(df.query(query).rename(columns=self._generate_rename_columns(perm)))
+        # determine which method to use for concatenation
+        if isinstance(df, pd.DataFrame):
+            result = pd.concat(subsets)
+        else:
+            result = dd.concat(subsets)
+        return result
+
+    def sort_labels(self, contacts:Contacts) -> Contacts:
+        """Sorts labels in ascending, alphabetical order"""
+        if not contacts.contains_meta_data:
+            raise ValueError("Sorting labels for unlabelled contacts is not implemented.")
+        # get label values. TODO: this should be a method of contacts
+        label_values = set()
+        for i in range(contacts.number_fragments):
+            label_values.update(contacts.data[f"meta_data_{i+1}"].unique())
+        # iterate over all permutations of label values
+        subsets = []
+        for perm in product(label_values, repeat=contacts.number_fragments):
+            query = " and ".join([f"meta_data_{i+1} == '{j}'" for i, j in enumerate(perm)])
+            desired_order = [i + 1 for i in np.argsort(perm)]
+            subsets.append(contacts.data.query(query).rename(columns=self._generate_rename_columns(desired_order)))
+        # determine which method to use for concatenation
+        if contacts.is_dask:
+            result = dd.concat(subsets).sort_index()
+        else:
+            result = pd.concat(subsets).sort_index()
+        return Contacts(result, number_fragments=contacts.number_fragments, label_sorted=True)
+
+
+    def subset_on_metadata(self, contacts:Contacts, metadata_combi: List[List[str]]) -> Contacts:
+        """Subset contacts based on metadata and sort label states.
+        If the metadata combination list contains more than one element, all
+        combinations will be assumed to be equivalent and renamed to the first
+        element in the list."""
+        # TODO
+
+
+    def flip_symmetric_contacts(self, contacts: Contacts) -> Contacts:
+        """Flips contacts based on inherent symmetry"""
+        if contacts.contains_meta_data and contacts.metadata_combi is None:
+            raise ValueError("""Flipping symmetry is only supported for pure metadata combinations.
+                             Either subset or pass to constructor.""")
+        if contacts.contains_meta_data:
+            raise NotImplementedError("Flipping symmetry for labelled contacts is not yet implemented.")
+        else:
+            result = self._flip_unlabelled_contacts(contacts.data)
+        return Contacts(result, number_fragments=contacts.number_fragments)
--- tests/test_symmetry.py
+++ tests/test_symmetry.py
@@ -1,9 +1,11 @@
+"""Tests for dealing with symmetry flipping for labelled and unlabelled contacts."""
+
 import pytest
 import pandas as pd
 import pandera as pa
 import numpy as np
 import dask.dataframe as dd
-from spoc.contacts import Contacts
+from spoc.contacts import Contacts, ContactManipulator
 
 @pytest.fixture
 def unlabelled_contacts_2d():
@@ -102,13 +104,122 @@ def unlabelled_contacts_3d_flipped():
         }
     )
 
+@pytest.fixture
+def labelled_binary_contacts_2d():
+    return pd.DataFrame(
+        {
+            "read_name": ["read1", "read2", "read3"],
+            "read_length": [100, 100, 100],
+            "chrom_1": ["chr1", "chr1", "chr1"],
+            "start_1": [100, 200, 300],
+            "end_1": [200, 300, 400],
+            "mapping_quality_1": [10, 10, 10],
+            "align_score_1": [10, 10, 10],
+            "align_base_qscore_1": [10, 10, 10],
+            "meta_data_1": ["A", "B", "A"],
+            "chrom_2": ["chr1", "chr1", "chr1"],
+            "start_2": [1000, 2000, 3000],
+            "end_2": [2000, 3000, 4000],
+            "mapping_quality_2": [10, 10, 15],
+            "align_score_2": [10, 10, 15],
+            "align_base_qscore_2": [10, 10, 15],
+            "meta_data_2": ["B", "A", "A"]
+        }
+    )
+
+@pytest.fixture
+def labelled_binary_contacts_2d_sorted():
+    return pd.DataFrame(
+        {
+            "read_name": ["read1", "read2", "read3"],
+            "read_length": [100, 100, 100],
+            "chrom_1": ["chr1", "chr1", "chr1"],
+            "start_1": [100, 2000, 300],
+            "end_1": [200, 3000, 400],
+            "mapping_quality_1": [10, 10, 10],
+            "align_score_1": [10, 10, 10],
+            "align_base_qscore_1": [10, 10, 10],
+            "meta_data_1": ["A", "A", "A"],
+            "chrom_2": ["chr1", "chr1", "chr1"],
+            "start_2": [1000, 200, 3000],
+            "end_2": [2000, 300, 4000],
+            "mapping_quality_2": [10, 10, 15],
+            "align_score_2": [10, 10, 15],
+            "align_base_qscore_2": [10, 10, 15],
+            "meta_data_2": ["B", "B", "A"]
+        }
+    )
+
+
+@pytest.fixture
+def labelled_binary_contacts_3d():
+    return pd.DataFrame(
+        {
+            "read_name": ["read1", "read2", "read3"],
+            "read_length": [100, 100, 100],
+            "chrom_1": ["chr1", "chr1", "chr1"],
+            "start_1": [100, 200, 300],
+            "end_1": [200, 300, 400],
+            "mapping_quality_1": [10, 10, 10],
+            "align_score_1": [10, 10, 10],
+            "align_base_qscore_1": [10, 10, 10],
+            "meta_data_1": ["A", "B", "A"],
+            "chrom_2": ["chr1", "chr1", "chr1"],
+            "start_2": [1000, 2000, 3000],
+            "end_2": [2000, 3000, 4000],
+            "mapping_quality_2": [10, 10, 15],
+            "align_score_2": [10, 10, 15],
+            "align_base_qscore_2": [10, 10, 15],
+            "meta_data_2": ["B", "A", "A"],
+            "chrom_3": ["chr1", "chr1", "chr1"],
+            "start_3": [1000, 2000, 3000],
+            "end_3": [2000, 3000, 4000],
+            "mapping_quality_3": [10, 10, 15],
+            "align_score_3": [10, 10, 15],
+            "align_base_qscore_3": [10, 10, 15],
+            "meta_data_3": ["B", "A", "A"]
+        }
+    )
+
+@pytest.fixture
+def labelled_binary_contacts_3d_sorted():
+    return pd.DataFrame(
+        {
+            "read_name": ["read1", "read2", "read3"],
+            "read_length": [100, 100, 100],
+            "chrom_1": ["chr1", "chr1", "chr1"],
+            "start_1": [100, 2000, 300],
+            "end_1": [200, 3000, 400],
+            "mapping_quality_1": [10, 10, 10],
+            "align_score_1": [10, 10, 10],
+            "align_base_qscore_1": [10, 10, 10],
+            "meta_data_1": ["A", "A", "A"],
+            "chrom_2": ["chr1", "chr1", "chr1"],
+            "start_2": [1000, 2000, 3000],
+            "end_2": [2000, 3000, 4000],
+            "mapping_quality_2": [10, 10, 15],
+            "align_score_2": [10, 10, 15],
+            "align_base_qscore_2": [10, 10, 15],
+            "meta_data_2": ["B", "A", "A"],
+            "chrom_3": ["chr1", "chr1", "chr1"],
+            "start_3": [1000, 200, 3000],
+            "end_3": [2000, 300, 4000],
+            "mapping_quality_3": [10, 10, 15],
+            "align_score_3": [10, 10, 15],
+            "align_base_qscore_3": [10, 10, 15],
+            "meta_data_3": ["B", "B", "A"]
+        }
+    )
+
+
+
 @pytest.mark.parametrize("unflipped, flipped",
                             [('unlabelled_contacts_2d', 'unlabelled_contacts_2d_flipped'),
                              ('unlabelled_contacts_3d', 'unlabelled_contacts_3d_flipped')])
 def test_unlabelled_contacts_flipped_correctly(unflipped, flipped, request):
     unflipped, flipped = request.getfixturevalue(unflipped), request.getfixturevalue(flipped)
     contacts = Contacts(unflipped)
-    flipped_contacts = contacts.flip_symmetric_contacts()
+    flipped_contacts = ContactManipulator().flip_symmetric_contacts(contacts)
     pd.testing.assert_frame_equal(flipped_contacts.data, flipped)
 
 @pytest.mark.parametrize("unflipped, flipped",
@@ -117,5 +228,16 @@ def test_unlabelled_contacts_flipped_correctly(unflipped, flipped, request):
 def test_unlabelled_contacts_flipped_correctly_dask(unflipped, flipped, request):
     unflipped, flipped = dd.from_pandas(request.getfixturevalue(unflipped), npartitions=1), request.getfixturevalue(flipped)
     contacts = Contacts(unflipped)
-    flipped_contacts = contacts.flip_symmetric_contacts()
+    flipped_contacts = ContactManipulator().flip_symmetric_contacts(contacts)
     pd.testing.assert_frame_equal(flipped_contacts.data.compute(), flipped)
+
+
+@pytest.mark.parametrize("unsorted, sorted_contacts",
+                            [('labelled_binary_contacts_2d', 'labelled_binary_contacts_2d_sorted'),
+                             ('labelled_binary_contacts_3d', 'labelled_binary_contacts_3d_sorted')])
+def test_labelled_contacts_are_sorted_correctly(unsorted, sorted_contacts, request):
+    unsorted, sorted_contacts = request.getfixturevalue(unsorted), request.getfixturevalue(sorted_contacts)
+    contacts = Contacts(unsorted)
+    result = ContactManipulator().sort_labels(contacts)
+    pd.testing.assert_frame_equal(result.data, sorted_contacts)
+    assert result.label_sorted
